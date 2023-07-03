bfeb8eaf8035d1256e97a3224898b2ee909a5d91
David Arnold <david.arnold@iohk.io>
2023-07-03 17:42:39 -0500

    feat: enable db socket connection

--- frappe/database/database.py
+++ frappe/database/database.py
@@ -80,6 +80,7 @@ class Database:
 
 	def __init__(
 		self,
+		socket,
 		host,
 		user,
 		password,
@@ -87,6 +88,7 @@ class Database:
 		cur_db_name,
 	):
 		self.setup_type_map()
+		self.socket = socket
 		self.host = host
 		self.port = port
 		self.user = user
